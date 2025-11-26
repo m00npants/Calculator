fd9c35e3b425c40145d52fa4677ea288422a4d0a
Andreas karlsson <andreas.k8505@gmail.com>
2025-11-26 11:02:22 +0100

    added a file with logics that the main file fetchs the info  from

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -20,4 +20,31 @@ public class Calculator {
         }
         return a / b;
     }
+
+
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public double sqrt(double value) {
+        if (value < 0) {
+            throw new ArithmeticException("Cannot take square root of negative number!");
+        }
+        return Math.sqrt(value);
+    }
+
+    public double modulus(double a, double b) {
+        return a % b;
+    }
+
+    public double factorial(int n) {
+        if (n < 0) {
+            throw new ArithmeticException("Factorial not defined for negative numbers!");
+        }
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,101 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+     static void main() {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calc = new Calculator();
+
+        boolean keepRunning = true;
+
+        while (keepRunning) {
+            System.out.print("Enter operator (+, -, *, /, ^, sqrt, %, !): ");
+            String operator = scanner.next();
+
+            try {
+                double result;
+
+                switch (operator) {
+                    case "+":
+                        System.out.print("Enter first number: ");
+                        double add1 = scanner.nextDouble();
+                        System.out.print("Enter second number: ");
+                        double add2 = scanner.nextDouble();
+                        result = calc.add(add1, add2);
+                        break;
+
+                    case "-":
+                        System.out.print("Enter first number: ");
+                        double sub1 = scanner.nextDouble();
+                        System.out.print("Enter second number: ");
+                        double sub2 = scanner.nextDouble();
+                        result = calc.subtract(sub1, sub2);
+                        break;
+
+                    case "*":
+                        System.out.print("Enter first number: ");
+                        double mul1 = scanner.nextDouble();
+                        System.out.print("Enter second number: ");
+                        double mul2 = scanner.nextDouble();
+                        result = calc.multiply(mul1, mul2);
+                        break;
+
+                    case "/":
+                        System.out.print("Enter first number: ");
+                        double div1 = scanner.nextDouble();
+                        System.out.print("Enter second number: ");
+                        double div2 = scanner.nextDouble();
+                        result = calc.divide(div1, div2);
+                        break;
+
+                    case "^":
+                        System.out.print("Enter base: ");
+                        double base = scanner.nextDouble();
+                        System.out.print("Enter exponent: ");
+                        double exp = scanner.nextDouble();
+                        result = calc.power(base, exp);
+                        break;
+
+                    case "sqrt":
+                        System.out.print("Enter number: ");
+                        double val = scanner.nextDouble();
+                        result = calc.sqrt(val);
+                        break;
+
+                    case "%":
+                        System.out.print("Enter first number: ");
+                        double mod1 = scanner.nextDouble();
+                        System.out.print("Enter second number: ");
+                        double mod2 = scanner.nextDouble();
+                        result = calc.modulus(mod1, mod2);
+                        break;
+
+                    case "!":
+                        System.out.print("Enter integer: ");
+                        int n = scanner.nextInt();
+                        result = calc.factorial(n);
+                        break;
+
+                    default:
+                        System.out.println("Invalid operator!");
+                        continue;
+                }
+
+                System.out.println("Result: " + result);
+
+            } catch (ArithmeticException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+            System.out.print("Do you want to continue? (yes/no): ");
+            String choice = scanner.next().toLowerCase();
+            if (!choice.equals("yes")) {
+                keepRunning = false;
+                System.out.println("Calculator exited.");
+            }
         }
+
+
     }
 }
